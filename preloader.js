cd5b59dd12e0cc04edb3113bc874f894470ee865
automaticflow <contato@automaticflow.com.br>
2024-09-28 13:39:25 -0300

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -8,8 +8,8 @@ overlay.style.cssText = `
     position: fixed;
     top: 0;
     left: 0;
-    width: 100%;
-    height: 100%;
+    width: 20%;
+    height: 20%;
     background-color: rgba(255, 255, 255, 0.7);
     z-index: 999;
 `;
